323b9fa7663d184f594ba191fb3c7665f9ab3726
Argel <argel.torres@cetys.edu.mx>
2016-09-10 21:44:57 -0700

    Solucion alternativa con import thread

--- Threading_Abarca.py
+++ Threading_Abarca.py
@@ -15,3 +15,23 @@ if __name__ == '__main__':
         mythread = MyThread(name = "Thread-{}".format(x + 1))  #Se instancia y se le asigna su identificacion
         mythread.start()                                   # Ahora si se inicia
         time.sleep(0.5)                                     # Esperamos medio segundo para generar otro
+
+#import time
+#import thread
+#def print_time( threadName,delay):
+#    count = 0
+#    while count < 5:
+#        time.sleep(delay)
+#        count += 1
+#        print "%s: %s" % (threadName,time.ctime(time.time()))
+#try:
+#    thread.start_new_thread(print_time,("Thread-1",2, ) )
+#    thread.start_new_thread(print_time,("Thread-2",2, ) )
+#    thread.start_new_thread(print_time, ("Thread-3", 2,))
+#    thread.start_new_thread(print_time, ("Thread-4", 2,))
+#    thread.start_new_thread(print_time, ("Thread-5", 2,))
+#except:
+#    print "I am Bagu"
+#while 1:
+#    pass
+
