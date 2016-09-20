ef03789d4246eb50b3995dc237d1f0fae8a4b6c5
KatiaGarcia96 <katia.garcia@cetys.edu.mx>
2016-09-20 09:25:28 -0700

    Avance 1

---
+++ PycharmProjects/Proyecto/Avance_1.py
@@ -0,0 +1,38 @@
+import os
+
+
+def get_size(the_path):
+    path_size = 0
+    for path, directories, files in os.walk(the_path):
+        for filename in files:
+            path_size += os.lstat(os.path.join(path, filename)).st_size
+        for directory in directories:
+            path_size += os.lstat(os.path.join(path, directory)).st_size
+    path_size += os.path.getsize(the_path)
+    return path_size
+
+def bytes2human(n):
+    symbols = ('K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y')
+    prefix = {}
+    for i, s in enumerate(symbols):
+        prefix[s] = 1 << (i+1)*10
+    for s in reversed(symbols):
+        if n >= prefix[s]:
+            value = float(n) / prefix[s]
+            return '%.1f%s' % (value, s)
+    return "%sB" % n
+
+def Datos_MapDisk():
+    rootDir = '/home'
+    content_list = []
+    for dirName, subdirList, fileList in os.walk(rootDir, topdown=False):
+        content_list.append(dirName)
+
+    size_list = []
+    for i in content_list:
+        size_list.append(bytes2human(get_size(i)))
+        print(i, bytes2human(get_size(i)))
+
+
+if __name__=="__main__":
+    Datos_MapDisk()
