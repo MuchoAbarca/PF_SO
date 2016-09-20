54bf11c648d30901bf49e837dcc7e257b5d474a2
MuchoAbarca <ricardo.abarca@cetys.edu.mx>
2016-09-20 12:26:28 -0700

    Agregué función para guardar la list de procesos

--- PycharmProjects/Proyecto/Avance_1.py
+++ PycharmProjects/Proyecto/Avance_1.py
@@ -33,6 +33,9 @@ def Datos_MapDisk():
         size_list.append(bytes2human(get_size(i)))
         print(i, bytes2human(get_size(i)))
 
+def Guardando():
+    os.system("ps aux > process_list.txt")
+    print ("Se lista de procesos se ha generado")
 
 if __name__=="__main__":
     Datos_MapDisk()
