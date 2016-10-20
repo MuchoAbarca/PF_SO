1cffa21823f3189e6b0af1ce0d8e8943e92650ea
leslie827 <leslie.deanda@cetys.edu.mx>
2016-10-19 17:42:26 -0700

    Update Avance_2.py

--- PycharmProjects/Proyecto/Avance_2.py
+++ PycharmProjects/Proyecto/Avance_2.py
@@ -4,13 +4,15 @@ import plotly.plotly as py
 from Tkinter import *
 import ttk
 from os import system
-from time import sleep
 from plotly.graph_objs import *
 from logging import handler
 from fnmatch import fnmatch
 from multiprocessing import Process
 import numpy as np
 import matplotlib.pyplot as plt
+import psutil
+import time
+
 
 root = '/'
 pattern = []
@@ -364,48 +366,40 @@ notebook.add(MDC, text='MapDisk de Carpetas')
 #Detectar el cambio de tab
 notebook.bind("<ButtonRelease-1>", TabChange)
 v0.mainloop()
-#import matplotlib.pyplot as plt
-#plt.ion()
-#class DynamicUpdate():
-#    #Suppose we know the x range
-#    min_x = 0
-#    max_x = 10
 
-#    def on_launch(self):
-#        #Set up plot
-#        self.figure, self.ax = plt.subplots()
-#        self.lines, = self.ax.plot([],[], 'o')
-#        #Autoscale on unknown axis and known lims on the other
-#        self.ax.set_autoscaley_on(True)
-        #self.ax.set_xlim(self.min_x, self.max_x)
-        #Other stuff
-#        self.ax.grid()
+#grafica de uso de procesador
+plt.ion()
+class GrafProcesador():
+    minimo = 0
+    maximo = 10
+
+    def Dibujar(self):
+        self.figura, self.ax = plt.subplots()
+        self.lineas, = self.ax.plot([],[], 'o')
+        self.ax.set_autoscaley_on(True)
+        self.ax.set_xlim(self.minimo, self.maximo)
+        self.ax.grid()
 
 
-#    def on_running(self, xdata, ydata):
-#        #Update data (with the new _and_ the old points)
-#        self.lines.set_xdata(xdata)
-#        self.lines.set_ydata(ydata)
-        #Need both of these in order to rescale
-#        self.ax.relim()
-#        self.ax.autoscale_view()
-        #We need to draw *and* flush
-#        self.figure.canvas.draw()
-#        self.figure.canvas.flush_events()
+    def Corriendo(self, xdata, ydata):
+        self.lineas.set_xdata(xdata)
+        self.lineas.set_ydata(ydata)
+        self.ax.relim()
+        self.ax.autoscale_view()
+        self.figura.canvas.draw()
+        self.figura.canvas.flush_events()
 
-    #Example
-#    def __call__(self):
-#        import numpy as np
-#        import time
-#        self.on_launch()
-#        xdata = []
-#        ydata = []
-#        for x in np.arange(0,1000,0.5):
-#            xdata.append(x)
-#            ydata.append(np.exp(-x**2)+10*np.exp(-(x-7)**2))
-#            self.on_running(xdata, ydata)
-#            time.sleep(1)
-#        return xdata, ydata
+    def __call__(self):
+        self.Dibujar()
+        xdata = []
+        ydata = []
+        for x in np.arange(0,100,0.5):
+            ydat= psutil.cpu_percent(interval=1, percpu=False)
+            xdata.append(x)
+            ydata.append(ydat)
+            self.Corriendo(xdata, ydata)
+            time.sleep(1)
+        return xdata, ydata
 
-#d = DynamicUpdate()
-#d()
+x = GrafProcesador()
+x()
